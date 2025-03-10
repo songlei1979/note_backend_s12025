09c25da9068fd73c627a7c2e2a6fe469d107e362
Lei <gabriel_sl19798@hotmail.com>
2025-03-10 16:26:49 +1300

    created login view, and fixed errors in registration

--- note_app/serialzers.py
+++ note_app/serialzers.py
@@ -6,3 +6,9 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
