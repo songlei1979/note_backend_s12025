aa5d0b2019683068d321d35935503f61b5ab756d
Lei <gabriel_sl19798@hotmail.com>
2025-03-13 10:36:03 +1300

    logout done

--- note_app/views.py
+++ note_app/views.py
@@ -5,7 +5,7 @@ from rest_framework import generics
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
 from note_app.serialzers import UserSerializer
@@ -21,3 +21,11 @@ class LoginView(ObtainAuthToken):
         response = super(LoginView, self).post(request, *args, **kwargs)
         token = Token.objects.get(key=response.data['token'])
         return Response({'token': token.key, 'user_id': token.user_id})
+
+
+
+class LogoutView(generics.GenericAPIView):
+    permission_classes = (IsAuthenticated,)
+    def post(self, request):
+        request.user.auth_token.delete()
+        return Response(status=200)
