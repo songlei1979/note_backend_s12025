ef0764f5ae2a419ef5642b1365cfe2e6a2dfe516
Lei <gabriel_sl19798@hotmail.com>
2025-03-17 15:14:14 +1300

    removed user path for testing action

--- note_app/urls.py
+++ note_app/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('user/', UserView.as_view(), name='user'),
 ]
